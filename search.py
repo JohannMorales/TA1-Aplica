b55366d81631972a57ef80ca771a305106912753
jm <johann.morales@pucp.edu.pe>
2017-09-14 08:17:50 -0500

    update

--- search.py
+++ search.py
@@ -104,37 +104,6 @@ def depthFirstSearch(problem):
             if not (child.state) in explored:
                 frontier.push(child)
 
-
-def depthFirstSearchReversed(problem):
-    node = Node()
-    node.state=problem.getStartState()
-    frontier=util.Stack()
-    frontier.push(node)
-    explored=Set([])
-
-    while True:
-        if frontier.isEmpty():
-            return []
-        node=frontier.pop()
-        if problem.isGoalState(node.state):
-            return node.solution
-
-        explored.add(node.state)
-
-        successors = problem.getSuccessors(node.state)
-        
-        for action in reversed(successors):
-            child=Node()
-            child.cost=node.cost + action[2]
-            child.solution=list(node.solution)
-            child.solution.append(action[1])
-            child.state=action[0]
-            if not (child.state) in explored:
-                frontier.push(child)
-
-def dummySearch(problem):
-    return []
-
 def breadthFirstSearch(problem):
     node = Node()
     node.state=problem.getStartState()
@@ -152,14 +121,14 @@ def breadthFirstSearch(problem):
         explored.append(node.state)
 
         successors = problem.getSuccessors(node.state)
-        random.shuffle(successors)
+
         for action in successors:
             child=Node()
             child.cost=node.cost + action[2]
             child.solution=list(node.solution)
             child.solution.append(action[1])
             child.state=action[0]
-            if not (child.state) in explored:
+            if not (child.state) in explored and not child in frontier.list:
                 frontier.push(child)
 
 def uniformCostSearch(problem):
@@ -185,4 +154,3 @@ bfs = breadthFirstSearch
 dfs = depthFirstSearch
 astar = aStarSearch
 ucs = uniformCostSearch
-dfsR = depthFirstSearchReversed
